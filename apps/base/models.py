c24bd413521bd4f83195efbd6e93fb0d29888393
Marlon Falcón Hernández <falconsoft.3d@gmail.com>
2023-01-07 20:39:39 +0100

    sc

---
+++ apps/base/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.conf import settings
+
+class ClassModel(models.Model):
+    state = models.BooleanField(default=True)
+    fc = models.DateTimeField(auto_now_add=True)
+    fm = models.DateTimeField(auto_now=True)
+    
+    uc = models.ForeignKey(User, on_delete=models.CASCADE )
+    um = models.IntegerField(blank=True, null=True)
+
+    class Meta:
+        abstract = True
