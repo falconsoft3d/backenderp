bbc1c878655a48189408f3e68af9e3c00af98606
Marlon Falcón Hernández <falconsoft.3d@gmail.com>
2023-01-07 21:52:02 +0100

    sc

--- backenderp/settings/defaults.py
+++ backenderp/settings/defaults.py
@@ -29,7 +29,8 @@ sys.path.append(os.path.join(BASE_DIR, "apps"))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+# SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = "1233323232"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
